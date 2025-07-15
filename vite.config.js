8bce2a8de426f8d8c975e289ef5e132af774d950
rhysonLin <95329915+rhysonLin@users.noreply.github.com>
2025-07-15 15:19:06 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import cesium from 'vite-plugin-cesium'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),cesium()],
+  base: '/HuaFa/', // 设置你的publicPath
+  plugins: [vue(), cesium()],
+  build: {
+    rollupOptions: {
+      output: {
+        // 确保Cesium资源被正确打包到指定目录
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.includes('cesium')) {
+            return 'cesium/[name]-[hash][extname]';
+          }
+          return '[name]-[hash][extname]';
+        },
+      }
+    }
+  }
 })
